a49fffae633ac7daae3ca0f4118e40b8a8b835c9
Jacob Hixon <jacobhixon47@gmail.com>
2018-09-10 19:23:44 -0500

    add functioning 'avatar' command and allow mentioning other users to get their avatar vs default display of author's avatar.

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,7 +1,25 @@
 module.exports.run = async (client, message, args) => {
-  console.log('avatar is working!');
+  if (message.mentions.users.first()) {
+    message.channel.send(`**${message.mentions.users.first().username}**'s avatar:`, {
+      embed: {
+        files: [{
+          attachment: message.mentions.users.first().displayAvatarURL,
+          name: 'avatar.jpg'
+        }]
+      }
+    }).catch(console.error);
+  } else {
+    message.channel.send(`**${message.author.username}**'s avatar:`, {
+      embed: {
+        files: [{
+          attachment: message.author.displayAvatarURL,
+          name: 'avatar.jpg'
+        }]
+      }
+    }).catch(console.error);
+  }
 }
 
 module.exports.help = {
-  name: 'avatar'
+  name: "avatar"
 }
